5eda43ad6e166bedf9a4a598f4428eef332424ff
Ihsanrabani <ztex2727@gmail.com>
2024-09-20 16:47:54 +0700

    Fungsi keseluruhan  TODOLIST

--- index.js
+++ index.js
@@ -131,9 +131,11 @@
     //   console.log("udah masuk undone");
     // }
 
+
+
 function formattedDate() {
   let today = new Date();
-  let day = today.getDate() + 1;
+  let day = today.getDate()
   let month = today.getMonth() + 1;
   let year = today.getFullYear();
 
@@ -145,24 +147,45 @@ function displayToDo(event) {
   event.preventDefault();
 
   let todoList = document.getElementById("todoList");
+  let inputName = document.querySelector("#taskName");
   let name = document.querySelector("#taskName").value;
   let prioritas = document.querySelector("#priority").value;
   let date = formattedDate();
 
   if (name !== "") {
+    console.log(prioritas)
+
     let newTask = document.createElement("li");
+    newTask.id = "Task"
+
+    newTask.classList.add("prioritas-" + prioritas);
 
     newTask.innerHTML = `
-      <p>Nama tugas: ${name}</p>
-      <p>Prioritas: ${prioritas}</p>
-      <p>Tanggal: ${date}</p>
-      <input type="checkbox" class="checkbox">
+      <div class="cb-wrapper">
+          <input type="checkbox" class="checkbox" id="cb">
+      </div>
+      <div class="info-wrapper">
+          <h1 class="title-todo">${name}</h1>
+
+          <p class="date-todo">${date}</p>
+      </div>
+      <div class="label-wrapper-${prioritas}">
+          <p class="label-todo">${prioritas}</p>
+      </div>
+      <div class="delBtn-wrapper"id="delWrapper">
+          <button class="del-btn" onclick="deleteTodoI()">X</button>
+      </div>
     `;
 
     todoList.appendChild(newTask);
-    inputTask.value = "";
+    inputName.value = "";
 
-    let taskCheckbox = newTask.querySelector(".checkbox");
+    let textNoName = document.querySelector(".no-task");
+    if (textNoName) {
+      TDL.removeChild(textNoName);
+    }
+
+    let taskCheckbox = newTask.querySelector(".checkbox");  
 
     // Event listener pada checkbox, khusus untuk task ini
     taskCheckbox.addEventListener("change", function () {
@@ -177,26 +200,106 @@ function displayToDo(event) {
 
 function moveTaskTODone(task) {
   let doneList = document.getElementById("doneList");
+  let toDoList = document.getElementById("todoList")
 
   // Pastikan elemen task dipindahkan hanya ke doneList jika belum ada di dalamnya
   if (!doneList.contains(task)) {
     doneList.appendChild(task);
     console.log("Task dipindahkan ke doneList");
+
+    let textNoName = document.querySelector(".no-task");
+    if (textNoName) {
+      doneList.removeChild(textNoName);
+    }
+  }
+
+  if (toDoList.children.length == 0) {
+    //cek apakah ada li di dalam ul
+    let noTask = document.createElement("p");
+    noTask.classList.add("no-task");
+    noTask.innerHTML = "Tidak Ada Tugas";
+    toDoList.appendChild(noTask);
+
+    console.log("kossong");
   }
 }
 
 function moveTaskTOTDL(task) {
   let todoList = document.getElementById("todoList");
+  let doneList = document.getElementById("doneList");
 
   // Pastikan elemen task dipindahkan kembali ke todoList jika belum ada di dalamnya
   if (!todoList.contains(task)) {
     todoList.appendChild(task);
     console.log("Task dipindahkan kembali ke todoList");
+
+    let textNoName = document.querySelector(".no-task");
+    if (textNoName) {
+      todoList.removeChild(textNoName);
+    }
+  }
+
+  if (doneList.children.length == 0) {
+    //cek apakah ada li di dalam ul
+    let noTask = document.createElement("p");
+    noTask.classList.add("no-task");
+    noTask.innerHTML = "Belum ada tugas yang selesai";
+    doneList.appendChild(noTask);
   }
 }
 
+let TDL = document.getElementById("todoList");
+let TDD = document.getElementById("doneList");
+
+// utk table TDL
+if (TDL.children.length == 0) {
+  //cek apakah ada li di dalam ul
+  let noTask = document.createElement("p");
+  noTask.classList.add("no-task");
+  noTask.innerHTML = "Tidak Ada Tugas"
+  TDL.appendChild(noTask);
+}
+
+// utk table TDD
+if (doneList.children.length == 0) {
+  //cek apakah ada li di dalam ul
+  let noTask = document.createElement("p");
+  noTask.classList.add("no-task");
+  noTask.innerHTML = "Belum ada tugas yang selesai";
+  doneList.appendChild(noTask);
+}
 
 
+function deleteTodo() {
+  let ulParent = document.getElementById("todoList");
+
+  while (ulParent.firstChild) { 
+    ulParent.removeChild(ulParent.firstChild);
+  }
+
+  if (TDL.children.length == 0) {
+    //cek apakah ada li di dalam ul
+    let noTask = document.createElement("p");
+    noTask.classList.add("no-task");
+    noTask.innerHTML = "Tidak Ada Tugas";
+    TDL.appendChild(noTask);
+  }
+}
+
+function deleteTodoI() {
+  let child = document.getElementById("delWrapper");
+
+  let parent = child.parentElement;
+
+  parent.remove()
+  if (TDL.children.length == 0) {
+    //cek apakah ada li di dalam ul
+    let noTask = document.createElement("p");
+    noTask.classList.add("no-task");
+    noTask.innerHTML = "Tidak Ada Tugas";
+    TDL.appendChild(noTask);
+  }
+}
 
 
 
